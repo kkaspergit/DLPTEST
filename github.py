703f1d6cb870005c80ecf763d4cf9a779eca9056
kkaspergit <33729427+kkaspergit@users.noreply.github.com>
2025-04-01 12:35:26 -0400

    removed unnecessary module imports

--- github.py
+++ github.py
@@ -6,11 +6,8 @@ import getpass
 import datetime
 import argparse
 from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
-#from selenium.webdriver.support.ui import WebDriverWait
-#from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 GitHubUser="kkaspergit"
